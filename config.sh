ec34327130667c0944a64283fd11cb31e60dc958
Wuxiangdong <w15607911028@outlook.com>
2015-08-26 17:01:07 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -9,9 +9,8 @@ sed -i '/127.0.0.1/s/$/ 'wuxiangdong-virtualbox'/' /etc/hosts
 sed -i '/::1/s/$/ 'wuxiangdong-virtualbox'/' /etc/hosts
 systemctl enable dhcpcd
 pacman -S --noconfirm intel-ucode
-pacman -S --noconfirm dosfstools efibootmgr
 pacman -S --noconfirm grub
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=arch_grub --recheck
+grub-install --recheck /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 echo  >> /etc/pacman.conf
 echo "[archlinuxcn]" >> /etc/pacman.conf
@@ -28,14 +27,12 @@ cp /usr/share/oh-my-zsh/zshrc /home/wuxiangdong/.zshrc
 echo "apple_9698" | passwd wuxiangdong
 echo "apple_9698" | passwd
 pacman -S --noconfirm xorg-server xorg-server-utils
-pacman -S --noconfirm xf86-video-intel mesa mesa-libgl lib32-mesa-libgl libva-intel-driver libva
+pacman -S --noconfirm xf86-video-intel mesa mesa-libgl libva-intel-driver libva
 pacman -S --noconfirm plasma
 pacman -S --noconfirm alsa-utils alsa-plugins
 pacman -S --noconfirm pulseaudio
 pacman -S --noconfirm libcanberra-pulse
 pacman -S --noconfirm xf86-input-synaptics
-pacman -S --noconfirm lightdm
-systemctl enable lightdm
-pacman -S --noconfirm lightdm-gtk-greeter
-nano /etc/lightdm/lightdm.conf
+pacman -S --noconfirm sddm sddm-kcm
+systemctl enable sddm
 pacman -S --noconfirm firefox firefox-i18n-zh-cn
